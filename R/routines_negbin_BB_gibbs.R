fa1ff027dbae09b845ce328f73b7086b8f009282
Lorenzo Ghilotti <lorenzoghilotti96@gmail.com>
2023-05-06 10:18:46 -0700

    geometric prior implemented: it works!

--- R/routines_negbin_BB_gibbs.R
+++ R/routines_negbin_BB_gibbs.R
@@ -144,7 +144,7 @@ compute_log_ratio_q_negbin <- function(nstar_hat_prop, s_hat_prop, alpha_bar_hat
 ############################################################################
 
 
-#' Metropolis-within-Gibbs sampler for BB + NegBin
+#' Metropolis-within-Gibbs sampler for BB + NegBin (Gamma prior on nstar)
 #'
 #' @param Z [integer] binary matrix of presence/absence (n x K- dimensional)
 #' @param nstar_0 [numeric] initial value of nstar
@@ -171,7 +171,7 @@ compute_log_ratio_q_negbin <- function(nstar_hat_prop, s_hat_prop, alpha_bar_hat
 #' @export
 #'
 #' @examples
-gibbs_sampler_negbin <- function(Z, 
+gibbs_sampler_negbin_gamma <- function(Z, 
                                 nstar_0, p_0, s_0, alpha_bar_0,
                                 a_star, b_star, alpha_p, beta_p, a_alpha, b_alpha, a_s, b_s,
                                 tau, fixed,
@@ -320,6 +320,321 @@ gibbs_sampler_negbin <- function(Z,
                "alpha_bar_vec" = alpha_bar_vec, "s_vec" = s_vec))
   
 }
+
+
+############################################################################
+############################################################################
+############################################################################
+
+#' Function the gradient of the log full-conditional for s_hat and alpha_bar_hat
+#' 
+#' @param nstar_hat
+#' @param s_hat 
+#' @param alpha_bar_hat 
+#' @param lambda 
+#' @param n 
+#' @param K 
+#' @param counts 
+#' @param a_s 
+#' @param b_s 
+#' @param a_alpha 
+#' @param b_alpha 
+#'
+#' @export
+#'
+compute_grad_log_full_negbin_geom <- function(s_hat, alpha_bar_hat, nstar, p,
+                                         n, K, counts, 
+                                         a_s, b_s, a_alpha, b_alpha){
+  
+  es <- exp(s_hat)
+  ea_bar <- exp(alpha_bar_hat)
+  
+  p1 <- exp(lgamma(n+es) + lgamma(es + ea_bar) -
+              lgamma(es) - lgamma(es + ea_bar +n) )
+  
+  # derivative wrt s_hat
+  
+  p2 <- exp( log(nstar + K) + log(1-p) - log(1/p1 -1 + p) )
+  
+  ds_hat <- a_s + es * (p2*(digamma(n+es) - digamma(es) - digamma(n + es + ea_bar) + 
+                              digamma(es + ea_bar)) - 
+                          K*(digamma(n+es+ea_bar) - digamma(es+ea_bar)) +
+                          sum(digamma(n - counts + es)) -
+                          K*digamma(es) - b_s)
+  
+  # derivative wrt alpha_bar_hat
+  dalpha_bar_hat <- K + a_alpha + ea_bar*( (digamma(n+es+ea_bar) - digamma(es+ea_bar))*
+                                             (- p2 - K) +
+                                             sum(digamma(counts + ea_bar)) -
+                                             K*digamma(1+ea_bar) - b_alpha)
+  
+  return (c(ds_hat, dalpha_bar_hat))
+}
+
+############################################################################
+
+
+#' Function computing the log ratio of the full-conditional of s_hat, alpha_bar_hat
+#' in proposed and current values
+#'
+#' @param s_hat_prop 
+#' @param alpha_bar_hat_prop 
+#' @param s_hat_curr 
+#' @param alpha_bar_hat_curr 
+#' @param lambda 
+#' @param n 
+#' @param K 
+#' @param counts 
+#' @param a_s 
+#' @param b_s 
+#' @param a_alpha 
+#' @param b_alpha 
+#'
+#' @export
+#'
+compute_log_ratio_full_negbin_geom <- function(s_hat_prop, alpha_bar_hat_prop,
+                                          s_hat_curr, alpha_bar_hat_curr, 
+                                          nstar, p, n, K, counts,  
+                                          a_s, b_s, a_alpha, b_alpha){
+  
+  es_prop <- exp(s_hat_prop)
+  ea_bar_prop <- exp(alpha_bar_hat_prop)
+  es_curr <- exp(s_hat_curr)
+  ea_bar_curr <- exp(alpha_bar_hat_curr)
+  
+  p1_prop <- exp(lgamma(n+es_prop) + lgamma(es_prop + ea_bar_prop) -
+                   lgamma(es_prop) - lgamma(es_prop + ea_bar_prop +n) )
+  p1_curr <- exp(lgamma(n+es_curr) + lgamma(es_curr + ea_bar_curr) -
+                   lgamma(es_curr) - lgamma(es_curr + ea_bar_curr +n) )
+  
+  v <- lgamma(ea_bar_prop + counts) - lgamma(1 + ea_bar_prop) + lgamma(es_prop+n - counts) -
+    lgamma(es_prop) - lgamma(ea_bar_curr + counts) + lgamma(1 + ea_bar_curr) - 
+    lgamma(es_curr+n - counts) + lgamma(es_curr)
+  
+  res <- K*(lgamma(n+es_curr+ea_bar_curr) + lgamma(es_prop + ea_bar_prop) -
+         lgamma(es_curr + ea_bar_curr) - lgamma(es_prop+ea_bar_prop+n)) -
+    (nstar + K)*(log(1-(1-p)*p1_prop) - log(1-(1-p)*p1_curr)) + 
+    sum(v) + 
+    (K+a_alpha)*(alpha_bar_hat_prop - alpha_bar_hat_curr) - b_alpha*(ea_bar_prop - ea_bar_curr) + 
+    a_s*(s_hat_prop - s_hat_curr) - b_s*(es_prop - es_curr) 
+  
+  
+  return(res)
+  
+}
+
+
+#############################################################################
+
+#' Function computing the log of the ratio of the terms related to q in MALA
+#'
+#' @param s_hat_prop 
+#' @param alpha_bar_hat_prop 
+#' @param s_hat_curr 
+#' @param alpha_bar_hat_curr 
+#' @param tau 
+#' @param grad_log_full_curr 
+#' @param grad_log_full_prop 
+#'
+#' @export
+#'
+compute_log_ratio_q_negbin_geom <- function(s_hat_prop, alpha_bar_hat_prop,
+                                       s_hat_curr, alpha_bar_hat_curr, 
+                                       tau, grad_log_full_curr, grad_log_full_prop){
+  
+  norm_prop <- norm(c(s_hat_curr, alpha_bar_hat_curr) - 
+                      c(s_hat_prop, alpha_bar_hat_prop) -
+                      tau*grad_log_full_prop, type="2")^2
+  
+  norm_curr <- norm( c(s_hat_prop, alpha_bar_hat_prop) - 
+                       c(s_hat_curr, alpha_bar_hat_curr) -
+                       tau*grad_log_full_curr, type="2")^2
+  
+  res <- 1/(4*tau)*(norm_curr - norm_prop)
+  
+  return (res)
+}
+
+######################################################################
+
+
+#' Metropolis-within-Gibbs sampler for BB + NegBin (Geometric prior on nstar)
+#'
+#' @param Z [integer] binary matrix of presence/absence (n x K- dimensional)
+#' @param nstar_0 [numeric] initial value of nstar
+#' @param p_0 [numeric] initial value of p
+#' @param s_0 [numeric] initial value of s
+#' @param alpha_bar_0 [numeric] initial value of alpha_bar 
+#' @param q_star [numeric] hyperparameter of Geometric prior on nstar
+#' @param alpha_p [numeric] 
+#' @param beta_p [numeric]
+#' @param a_alpha [numeric]
+#' @param b_alpha [numeric]
+#' @param a_s [numeric]
+#' @param b_s [numeric]
+#' @param tau [numeric] MALA step-size
+#' @param fixed [logical] vector indicating if the variable is fixed or has prior:
+#' if fixed, it stays equal to initial value
+#' @param S [integer] number of iterations for the MCMC algorithm
+#' @param n_burnin [integer] number of iterations for the burn-in
+#' @param thin [integer] thinning
+#' @param seed [integer] seed
+#'
+#' @return
+#' @export
+#'
+#' @examples
+gibbs_sampler_negbin_geometric <- function(Z, 
+                                 nstar_0, p_0, s_0, alpha_bar_0,
+                                 q_star, alpha_p, beta_p, a_alpha, b_alpha, a_s, b_s,
+                                 tau, fixed,
+                                 S, n_burnin, thin, seed){
+  
+  set.seed(seed)
+  
+  # Compute total number of sites
+  n <- nrow(Z)
+  
+  # Delete NA
+  Z <- Z[, colSums(is.na(Z))==0]
+  
+  # Delete zero-columns
+  Z <- Z[, colSums(Z)!=0]
+  
+  # Set K to be the observed number of features
+  K <- ncol(Z)
+  
+  # Compute vector of counts
+  counts <- colSums(Z)
+  
+  ############## Gibbs-sampler ##########################
+  
+  # Define structure to store parameters along the iterations
+  number_saved_iterations <- (S - n_burnin)/thin + 1
+  nstar_vec <- vector(length = number_saved_iterations)
+  p_vec <- vector(length = number_saved_iterations)
+  alpha_bar_vec <- vector(length = number_saved_iterations)
+  s_vec <- vector(length = number_saved_iterations)
+  
+  # Set initial values
+  nstar <- nstar_0
+  p <- p_0
+  alpha_bar <- alpha_bar_0
+  s <- s_0
+  
+  # index saved iterations (after burn-in and thinning satisfied)
+  l <- 1
+  
+  for (q in 1:S){
+    
+    t <- lgamma(s + n) + lgamma(s + alpha_bar) - lgamma(s) - lgamma(s+alpha_bar+n)
+    
+    ################################################################
+    ############# Draw p | Z, alpha_bar, s, nstar ###################
+    ###############################################################
+    
+    if (fixed[2] == FALSE){
+      # Update Ntilde | p, Z, alpha_bar, s, nstar
+      
+      Ntilde <- K + rnbinom(1, size = nstar + K, prob = 1 - (1-p)* exp(t))
+      
+      # Update p | Ntilde, Z, alpha_bar, s, nstar
+      
+      p <- rbeta(1, shape1 = nstar + alpha_p, shape2 = Ntilde + beta_p)
+      
+    }
+    
+    ################################################################
+    ############# Draw nstar | Z, alpha_bar, s, p ###################
+    ###############################################################
+    
+    if (fixed[1] == FALSE){
+      nstar <- 1 + rnbinom(1, size = K +1, 1 - ((1-q_star)*p)/(1 - (1-p)*exp(t)) )
+    }
+    
+    ################################################################
+    ############# Draw ( s, alpha_bar) | Z, p, nstar ##################
+    ###############################################################
+    
+    # In order to update s, alpha_bar, we update s_hat, alpha_bar_hat, 
+    # defined as the logarithm of s, alpha_bar
+    
+    ### Current values for s_hat, alpha_bar_hat
+    s_hat_curr <- log(s)
+    alpha_bar_hat_curr <- log(alpha_bar)
+    
+    ### Propose values for s_hat, alpha_bar_hat
+    # Compute the gradient of log-full conditional for MALA 
+    # (the order of returned variables is: s_hat, alpha_bar_hat)
+    grad_log_full_curr <- compute_grad_log_full_negbin_geom(s_hat_curr, alpha_bar_hat_curr, 
+                                                       nstar, p, n, K, counts, 
+                                                       a_s, b_s, a_alpha, b_alpha)
+    
+    if (fixed[3] == TRUE){
+      s_hat_prop <- s_hat_curr
+    } else {
+      s_hat_prop <- s_hat_curr + tau*grad_log_full_curr[1] + sqrt(2*tau)*rnorm(1)
+    }
+    
+    if (fixed[4] == TRUE){
+      alpha_bar_hat_prop <- alpha_bar_hat_curr
+    } else {
+      alpha_bar_hat_prop <- alpha_bar_hat_curr + tau*grad_log_full_curr[2] + sqrt(2*tau)*rnorm(1)
+    }
+    
+    ### Acceptance probability 
+    # Compute the log ratio of the full-cond in prop point and curr point
+    log_ratio_full <- compute_log_ratio_full_negbin_geom(s_hat_prop, alpha_bar_hat_prop,
+                                                    s_hat_curr, alpha_bar_hat_curr, 
+                                                    nstar, p, n, K, counts,   
+                                                    a_s, b_s, a_alpha, b_alpha)
+    
+    # Compute the log ratio of the terms related to the proposal q
+    grad_log_full_prop <- compute_grad_log_full_negbin_geom(s_hat_prop, alpha_bar_hat_prop, 
+                                                       nstar, p, n, K, counts, 
+                                                       a_s, b_s, a_alpha, b_alpha)
+    
+    log_ratio_q <- compute_log_ratio_q_negbin_geom(s_hat_prop, alpha_bar_hat_prop,
+                                              s_hat_curr, alpha_bar_hat_curr, tau,
+                                              grad_log_full_curr, grad_log_full_prop)
+    
+    # Compute acceptance probability
+    log_acc_prob <- log_ratio_full + log_ratio_q
+    acc_prob <- min(1, exp(log_acc_prob))
+    
+    # Decide if accept or not the new parameter vector
+    if (runif(1) < acc_prob){ # accept
+      s <- exp(s_hat_prop)
+      alpha_bar <- exp(alpha_bar_hat_prop)
+    }
+    
+    
+    #######################################################################
+    
+    # Store parameters if burn-in is over and once every "thin" iteration
+    if ((q > n_burnin) & (q %% thin == 0) ){
+      print(paste0("iteration: ", q))
+      
+      nstar_vec[l] <- nstar
+      p_vec[l] <- p
+      alpha_bar_vec[l] <- alpha_bar
+      s_vec[l] <- s
+      
+      l <- l+1
+    }
+    
+  }
+  
+  nstar_vec <- nstar_vec[1:(l-1)]
+  p_vec <- p_vec[1:(l-1)]
+  alpha_bar_vec <- alpha_bar_vec[1:(l-1)]
+  s_vec <- s_vec[1:(l-1)]
+  
+  return (list("nstar_vec" = nstar_vec, "p_vec" = p_vec, 
+               "alpha_bar_vec" = alpha_bar_vec, "s_vec" = s_vec))
+  
+}
+
 
 
 
--- gibbs_negbin_simulation.R
+++ gibbs_negbin_simulation.R
@@ -4,8 +4,8 @@ seed = 1234
 # set true parameters of the poiss BB process
 alpha <- -1
 theta <- 10
-nstar <- 10
-p <- 0.05
+nstar <- 50
+p <- 0.1
 # compute the corresponding mean and variance of the negbin
 mu <- nstar*(1-p)/p
 sigma2 <- nstar*(1-p)/(p**2)
@@ -22,6 +22,10 @@ data_list <- data_list_full$features
 data_mat <- create_features_matrix(data_list)
 
 ncol(data_mat)
+
+##############################################################################
+############################## GAMMA PRIOR ###################################
+##############################################################################
 
 # Set prior hyperparameters
 a_star <- 1
@@ -61,7 +65,7 @@ n_burnin <- 0
 thin <- 10
 seed <- 1234
 
-output <- gibbs_sampler_negbin(Z = data_mat,
+output <- gibbs_sampler_negbin_gamma(Z = data_mat,
                                nstar_0, p_0 = 0.01,  s_0 = 9 , alpha_bar_0 = 1,
                                a_star, b_star, alpha_p, beta_p, a_alpha, b_alpha, a_s, b_s,
                                tau, fixed = c(F, T,T,T),
@@ -96,3 +100,89 @@ theta
 nstar
 p 
 
+##########################################################################
+################# GEOMETRIC PRIOR #######################################
+##########################################################################
+
+# Set prior hyperparameters
+q_star <- 0.01
+print(paste0("E(nstar) = ", 1/ q_star))
+print(paste0("Var(nstar) = ", (1-q_star)/ (q_star^2)) )
+alpha_p <- 0.000001
+beta_p <- 0.000001
+print(paste0("E(p) = ", alpha_p/(alpha_p + beta_p)))
+print(paste0("Var(p) = ",(alpha_p*beta_p)/((alpha_p + beta_p)^2 *(alpha_p+beta_p+1)) ))
+a_alpha <- 10
+b_alpha <- 1
+print(paste0("E(alpha_bar) = ", a_alpha/ b_alpha))
+print(paste0("Var(alpha_bar) = ", a_alpha/ (b_alpha^2)))
+a_s <- 2
+b_s <- 0.1
+print(paste0("E(s) = ", a_s/ b_s))
+print(paste0("Var(s) = ", a_s/ (b_s^2)))
+
+print(paste0("E(theta) = ", a_s/ b_s + a_alpha/ b_alpha))
+print(paste0("Var(theta) = ", a_s/ (b_s^2) + a_alpha/ (b_alpha^2)))
+
+
+# Set initial values for the parameters
+nstar_0 <- 100
+p_0 <- 0.2
+alpha_bar_0 <- 1
+s_0 <- 9
+
+# Set tau for MALA
+# tau <- 0.001 # when nstar is fixed
+tau <- 0.00008
+
+# Set MCMC parameters
+S <- 8*10^4
+n_burnin <- 10^4
+thin <- 10
+seed <- 1234
+
+output <- gibbs_sampler_negbin_geometric(Z = data_mat,
+                                     nstar_0 = 100, p_0 = 0.2,  s_0 = 20, alpha_bar_0 = 5,
+                                     q_star, alpha_p, beta_p, a_alpha, b_alpha, a_s, b_s,
+                                     tau, fixed = c(F,F,F,F),
+                                     S, n_burnin, thin, seed)
+
+n_saved_iter <- length(output$nstar_vec)
+nstar_chain <- output$nstar_vec
+p_chain <- output$p_vec
+s_chain <- output$s_vec
+alpha_bar_chain <- output$alpha_bar_vec
+alpha_chain <- - alpha_bar_chain
+theta_chain <- s_chain + alpha_bar_chain
+
+################################################################
+############# Processing outputs ##############################
+################################################################
+
+# Mixing checks for (nstar, p, s, alpha_bar)
+plot(nstar_chain, type="l") # mixing of nstar in prior number of features
+plot(p_chain, type="l") # mixing of p in prior number of features
+plot(s_chain, type="l") # mixing of "s" 
+plot(alpha_bar_chain, type="l") # mixing of "alpha_bar" 
+
+# Mixing checks for (nstar, p, alpha, theta)
+plot(nstar_chain, type="l") # mixing of nstar in prior number of features
+# Running mean
+plot(cumsum(nstar_chain)/(1:n_saved_iter), type="l")
+
+plot(p_chain, type="l") # mixing of p in prior number of featuresplot(alpha_chain, type="l") # mixing of "alpha" 
+# Running mean
+plot(cumsum(p_chain)/(1:n_saved_iter), type="l")
+
+plot(theta_chain, type="l") # mixing of "theta" in the beta prior on weights
+# Running mean
+plot(cumsum(theta_chain)/(1:n_saved_iter), type="l")
+
+plot(alpha_chain, type="l") # mixing of "alpha" 
+# Running mean
+plot(cumsum(alpha_chain)/(1:n_saved_iter), type="l")
+
+alpha 
+theta 
+nstar
+p 
