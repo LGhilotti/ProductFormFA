76e2a8b09b482ea21d6bbcc965c4d761734fc71d
Lorenzo Ghilotti <lorenzoghilotti96@gmail.com>
2023-07-25 18:56:08 +0200

    routines for sb-sp written, to be tested

--- R/routines_gamma_IBP_gibbs.R
+++ R/routines_gamma_IBP_gibbs.R
@@ -50,6 +50,42 @@ compute_grad_log_full_gamma_ibp <- function(s_hat, alpha_hat, Gam,
 
 ############################################################################
 
+#' Function the gradient of the log full-conditional for s_hat and alpha_bar_hat
+#' 
+#' @param n 
+#' @param K 
+#' @param counts 
+#' @param a_alpha 
+#' @param b_alpha 
+#'
+#' @export
+#'
+compute_der_log_full_sb_sp <- function(alpha_hat, 
+                                       c, beta, Gam, n, K, counts,
+                                       a_alpha, b_alpha){
+  
+  ealpha <- exp(alpha_hat)
+  
+  p2 <- exp( lgamma((1:n)) - 
+               lgamma(1 - ealpha/(1+ealpha) + (1:n)) + lgamma(2 - ealpha/(1+ealpha) ) )
+  
+  # contribution of the Gamma-IBP
+  dalpha_hat <- a_alpha + ealpha/((1 + ealpha)^2) *
+    ( K*(digamma(1 - ealpha/(1+ealpha) + n ) - digamma(2 - ealpha/(1+ealpha) ) ) -
+        Gam*sum( p2 * (digamma(1 - ealpha/(1+ealpha) + (1:n)) - digamma(2 - ealpha/(1+ealpha) ))) -
+        sum(digamma(1/(1 + ealpha) + counts - 1)) + 
+        K*digamma(1/(1+ealpha)) - 
+        (a_alpha + b_alpha)*(1+ ealpha))
+  
+  # due to the fact alpha is in the prior on Gam
+  dalpha_hat <- dalpha_hat - (c + 1) + Gam*beta/ealpha
+  
+  return (dalpha_hat)
+  
+}
+
+
+#############################################################################
 
 #' Function computing the log ratio of the full-conditional of s_hat, alpha_bar_hat
 #' in proposed and current values
@@ -110,6 +146,57 @@ compute_log_ratio_full_gamma_ibp <- function(s_hat_prop, alpha_hat_prop,
 
 #############################################################################
 
+#' Function computing the log ratio of the full-conditional of s_hat, alpha_bar_hat
+#' in proposed and current values
+#'
+#' @param Gam 
+#' @param n 
+#' @param K 
+#' @param counts 
+#' @param a_alpha 
+#' @param b_alpha 
+#'
+#' @export
+#'
+compute_log_ratio_full_sb_sp <- function(alpha_hat_prop,
+                                         alpha_hat_curr, 
+                                         c, beta, Gam, n, K, counts,
+                                         a_alpha, b_alpha){
+  
+  ealpha_prop <- exp(alpha_hat_prop)
+  ealpha_curr <- exp(alpha_hat_curr)
+  
+  p2_prop <- exp( lgamma( (1:n) ) - 
+                    lgamma(1 - ealpha_prop/(1+ealpha_prop) + (1:n)) + 
+                    lgamma(2 - ealpha_prop/(1+ealpha_prop) ) )
+  
+  p2_curr <- exp( lgamma( (1:n) ) - 
+                    lgamma(1 - ealpha_curr/(1+ealpha_curr) + (1:n)) + 
+                    lgamma(2 - ealpha_curr/(1+ealpha_curr) ) )
+  
+  v <- lgamma(1/(1 + ealpha_prop) + counts - 1) - lgamma(1/(1 + ealpha_prop)) -
+    lgamma(1/(1 + ealpha_curr) + counts - 1) + lgamma(1/(1 + ealpha_curr)) 
+  
+  # log-ratio of the Gamma-IBP contribution
+  res <- K*(lgamma(2 - ealpha_prop/(1+ealpha_prop) ) - 
+              lgamma(1 - ealpha_prop/(1+ealpha_prop) + n) -
+              lgamma(2 - ealpha_curr/(1+ealpha_curr) ) + 
+              lgamma(1 - ealpha_curr/(1+ealpha_curr) + n)) -
+    Gam*sum(p2_prop - p2_curr) +
+    sum(v) + 
+    a_alpha*(alpha_hat_prop - alpha_hat_curr) - 
+    (a_alpha + b_alpha)*(log(1 + ealpha_prop) - log(1 + ealpha_curr))
+  
+  # due to the fact alpha is in prior on Gam
+  res <- res + (c+1)*(alpha_hat_curr - alpha_hat_prop) + 
+    Gam*beta*(1/ealpha_curr - 1/ealpha_prop)
+  
+  
+  return (res)
+  
+}
+#############################################################################
+
 #' Function computing the log of the ratio of the terms related to q in MALA
 #'
 #' @param s_hat_prop 
@@ -139,6 +226,34 @@ compute_log_ratio_q_gamma_ibp <- function(s_hat_prop, alpha_hat_prop,
   return (res)
 }
 
+
+######################################################################
+
+#' Function computing the log of the ratio of the terms related to q in MALA
+#'
+#' @param s_hat_prop 
+#' @param alpha_hat_prop 
+#' @param s_hat_curr 
+#' @param alpha_hat_curr 
+#' @param tau 
+#' @param grad_log_full_curr 
+#' @param grad_log_full_prop 
+#'
+#' @export
+#'
+compute_log_ratio_q_sb_sp <- function(alpha_hat_prop,
+                                      alpha_hat_curr, tau,
+                                      der_log_full_curr, der_log_full_prop){
+  
+  norm_prop <- (alpha_hat_curr - alpha_hat_prop - tau*der_log_full_prop)^2
+  
+  norm_curr <- (alpha_hat_prop - alpha_hat_curr - tau*der_log_full_curr)^2
+  
+  res <- 1/(4*tau)*(norm_curr - norm_prop)
+  
+  return (res)
+  
+}
 ######################################################################
 
 
@@ -319,6 +434,179 @@ gibbs_sampler_gamma_ibp <- function(Z,
   
   return (list("a_vec" = a_vec, "b_vec" = b_vec, 
                "alpha_vec" = alpha_vec, "s_vec" = s_vec))
+  
+}
+
+
+
+
+######################################################################
+
+
+#' Metropolis-within-Gibbs sampler for BB + NegBin (Geometric prior on nstar)
+#'
+#' @param Z [integer] binary matrix of presence/absence (n x K- dimensional)
+#' @param a_0 [numeric] initial value of a
+#' @param b_0 [numeric] initial value of b
+#' @param s_0 [numeric] initial value of s
+#' @param alpha_0 [numeric] initial value of alpha 
+#' @param p [numeric] hyperparameter of Geometric prior on a
+#' @param r [numeric] 
+#' @param t [numeric]
+#' @param a_alpha [numeric]
+#' @param b_alpha [numeric]
+#' @param a_s [numeric]
+#' @param b_s [numeric]
+#' @param tau [numeric] MALA step-size
+#' @param fixed [logical] vector indicating if the variable is fixed or has prior:
+#' if fixed, it stays equal to initial value - order: a, b, alpha, s
+#' @param S [integer] number of iterations for the MCMC algorithm
+#' @param n_burnin [integer] number of iterations for the burn-in
+#' @param thin [integer] thinning
+#' @param seed [integer] seed
+#'
+#' @return
+#' @export
+#'
+#' @examples
+gibbs_sampler_sb_sp <- function(Z, 
+                                c_0, beta_0, alpha_0,
+                                p, r, t, a_alpha, b_alpha, 
+                                tau, fixed,
+                                S, n_burnin, thin, seed){
+  
+  set.seed(seed)
+  
+  # Compute total number of sites
+  n <- nrow(Z)
+  
+  # Delete NA
+  Z <- Z[, colSums(is.na(Z))==0]
+  
+  # Delete zero-columns
+  Z <- Z[, colSums(Z)!=0]
+  
+  # Set K to be the observed number of features
+  K <- ncol(Z)
+  
+  # Compute vector of counts
+  counts <- colSums(Z)
+  
+  ############## Gibbs-sampler ##########################
+  
+  # Define structure to store parameters along the iterations
+  number_saved_iterations <- (S - n_burnin)/thin + 1
+  c_vec <- vector(length = number_saved_iterations)
+  beta_vec <- vector(length = number_saved_iterations)
+  alpha_vec <- vector(length = number_saved_iterations)
+
+  # Set initial values
+  c <- c_0
+  beta <- beta_0
+  alpha <- alpha_0
+
+  # index saved iterations (after burn-in and thinning satisfied)
+  l <- 1
+  
+  for (q in 1:S){
+    
+    ################################################################
+    ################# Draw c, beta, alpha | Z   ###################
+    ###############################################################
+    
+    # Update Gam | Z, alpha, c, beta
+    
+    gamma_a_1ma_n <- sum(exp(lgamma((1:n)) - 
+                             lgamma(1 - alpha + (1:n)) + lgamma(2 - alpha) ) )
+    
+    Gam <- rgamma(1, shape = K + c + 1, rate = beta*(1-alpha)/alpha + gamma_a_1ma_n)
+    
+    if (fixed[1] == FALSE){
+      
+      # Update c | Gam, Z, alpha, beta
+      
+      c <- rpois(1, beta*Gam*(1-p)*(1-alpha)/alpha )
+      
+    }
+    
+    if (fixed[2] == FALSE){
+      
+      # Update beta | Gam, Z, alpha, c
+      
+      beta <- rgamma(1, shape = c + 1 + r, rate = Gam*(1-alpha)/alpha + t )
+      
+    }
+    
+    
+    ################################################################
+    ############# Draw alpha | Z, Gam, c, beta ##################
+    ###############################################################
+    
+    # In order to update alpha, we update alpha_hat, 
+    # defined as transformation of alpha
+    
+    ### Current values for alpha_hat
+    alpha_hat_curr <- log(alpha/(1-alpha))
+    
+    ### Propose values for alpha_hat
+    # Compute the gradient of log-full conditional for MALA 
+    der_log_full_curr <- compute_der_log_full_sb_sp(alpha_hat_curr, 
+                                                     c, beta, Gam, n, K, counts,
+                                                     a_alpha, b_alpha)
+    
+    if (fixed[3] == TRUE){
+      alpha_hat_prop <- alpha_hat_curr
+    } else {
+      alpha_hat_prop <- alpha_hat_curr + tau*der_log_full_curr + sqrt(2*tau)*rnorm(1)
+    }
+    
+    ### Acceptance probability 
+    # Compute the log ratio of the full-cond in prop point and curr point
+    log_ratio_full <- compute_log_ratio_full_sb_sp(alpha_hat_prop,
+                                                   alpha_hat_curr, 
+                                                   c, beta, Gam, n, K, counts,
+                                                   a_alpha, b_alpha)
+    
+    # Compute the log ratio of the terms related to the proposal q
+    der_log_full_prop <- compute_der_log_full_sb_sp(alpha_hat_prop,
+                                                    c, beta, Gam, n, K, counts,
+                                                    a_alpha, b_alpha)
+    
+    log_ratio_q <- compute_log_ratio_q_sb_sp(alpha_hat_prop,
+                                             alpha_hat_curr, tau,
+                                             der_log_full_curr, der_log_full_prop)
+    
+    # Compute acceptance probability
+    log_acc_prob <- log_ratio_full + log_ratio_q
+    acc_prob <- min(1, exp(log_acc_prob))
+    
+    # Decide if accept or not the new alpha
+    if (runif(1) < acc_prob){ # accept
+      alpha <- exp(alpha_hat_prop)/(1 + exp(alpha_hat_prop))
+    }
+    
+    
+    #######################################################################
+    
+    # Store parameters if burn-in is over and once every "thin" iteration
+    if ((q > n_burnin) & (q %% thin == 0) ){
+      print(paste0("iteration: ", q))
+      
+      c_vec[l] <- c
+      beta_vec[l] <- beta
+      alpha_vec[l] <- alpha
+
+      l <- l+1
+    }
+    
+  }
+  
+  c_vec <- c_vec[1:(l-1)]
+  beta_vec <- beta_vec[1:(l-1)]
+  alpha_vec <- alpha_vec[1:(l-1)]
+
+  return (list("c_vec" = c_vec, "beta_vec" = beta_vec, 
+               "alpha_vec" = alpha_vec))
   
 }
 
